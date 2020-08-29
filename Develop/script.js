a3646ddf9074ccee6cc8f3ea7b12a1d5865ce461
arbdt <67365149+arbdt@users.noreply.github.com>
2020-08-29 14:44:45 +0930

    added arrays of valid characters to script.js along with empty generatePassword() function

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//generated password function code goes here
+function generatePassword(){
+  // arrays to hold character variants
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numeralArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var specialChars = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~"];
+
+  
+}
