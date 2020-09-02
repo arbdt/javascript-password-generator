ec99304e5364b7afefe5488d60dd44251d3cec5d
arbdt <67365149+arbdt@users.noreply.github.com>
2020-09-02 20:01:52 +0930

    reordered content of script.js to more clearly separate my additional code from the existing working code

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Assignment Code (EXISTING, UNTOUCHED)
 var generateBtn = document.querySelector("#generate");
 
-//variables
-var passwordLength = 0; // to store chosen length
-
-// arrays to hold character variants
-var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeralArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specialChars = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~"];
-
 // Write password to the #password input (EXISTING, UNTOUCHED)
 function writePassword() {
     var password = generatePassword();
@@ -20,6 +11,16 @@ function writePassword() {
 
 // Add event listener to generate button (EXISTING, UNTOUCHED)
 generateBtn.addEventListener("click", writePassword);
+
+// MY CONTENT STARTS HERE //
+//variables
+var passwordLength = 0; // to store chosen length
+
+// arrays to hold character variants
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeralArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialChars = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~"];
 
 //function to check for valid password length input
 function askLength(){
